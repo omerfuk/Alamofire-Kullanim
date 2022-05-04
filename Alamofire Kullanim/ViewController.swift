04dd3fe82e9b5d3f00516f786005def94a777ae1
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-04 12:07:23 +0300

    Initial Commit

---
+++ Alamofire Kullanim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alamofire Kullanim
+//
+//  Created by Ömer Faruk Kılıçaslan on 4.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
